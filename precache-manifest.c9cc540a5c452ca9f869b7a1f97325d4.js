2ed263b079e7e461f8ab6dd08edee0b8327a1ee0
Ragunandhan M <mragunandhan@gmail.com>
2020-01-17 14:17:08 +0530

    Updates

---
+++ precache-manifest.c9cc540a5c452ca9f869b7a1f97325d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a748f6b8a00617356cad9a4a630c172b",
+    "url": "/instructor-v1/index.html"
+  },
+  {
+    "revision": "049ca8506bda8fc42a02",
+    "url": "/instructor-v1/static/css/main.a7c921d5.chunk.css"
+  },
+  {
+    "revision": "99079cbca6a006f8b971",
+    "url": "/instructor-v1/static/js/2.266193ac.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/instructor-v1/static/js/2.266193ac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "049ca8506bda8fc42a02",
+    "url": "/instructor-v1/static/js/main.c988b492.chunk.js"
+  },
+  {
+    "revision": "dab6913d9f24070bb135",
+    "url": "/instructor-v1/static/js/runtime-main.a34c822a.js"
+  }
+]);
